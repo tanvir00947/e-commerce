10f351d79d926265c20edda0005c12e6085f866d
tanvir00947 <tanvir00947@gmail.com>
2023-12-02 11:22:57 +0530

    added order_status

--- store/models.py
+++ store/models.py
@@ -36,6 +36,7 @@ class Order(models.Model):
 	date_ordered = models.DateTimeField(auto_now_add=True)
 	complete = models.BooleanField(default=False)
 	transaction_id = models.CharField(max_length=100, null=True)
+	order_status=models.CharField(max_length=200,null=True,default="Processing")
 
 	def __str__(self):
 		return str(self.id)+" "+self.customer.name+" "+str(self.transaction_id)
